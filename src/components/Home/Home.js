78fb0d303f463fa972b21d41970d0f98d0e51d86
Todd Spainhour <toddspainhour@gmail.com>
2020-06-07 15:06:52 -0500

    edit junk route works

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,10 +3,18 @@ import React from 'react';
 import './Home.scss';
 
 class Home extends React.Component {
+  editEvent = (e) => {
+    e.preventDefault();
+    const junkId = 'junk1'
+    this.props.history.push(`/edit/${junkId}`);
+
+  }
+
   render() {
     return (
       <div className="Home">
         <h4>Home component</h4>
+        <button className="btn btn-danger" onClick={this.editEvent}>Edit the Junk</button>
       </div>
     );
   }
